24a802b1deb9a5c8e2d36beba840284de96d5660
Niwahereza-Trust <niwaherezatrust6@gmail.com>
2026-07-03 13:13:33 +0300

    removed the imports from app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import Hero from './components/Hero'
 import About from './components/About'
 import Skills from './components/Skills'
 import Projects from './components/Projects'
-import Education from './components/Education'
 import Contact from './components/Contact'
 import Footer from './components/Footer'
 import './index.css'
@@ -16,7 +15,6 @@ function App() {
       <About />
       <Skills />
       <Projects />
-      <Education />
       <Contact />
       <Footer />
     </>
